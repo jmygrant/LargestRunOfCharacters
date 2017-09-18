f57069cd0520ba075d2e241c453b9a6cc8d860e1
Jared Mygrant <jamrow@gmail.com>
2017-09-18 10:08:02 -0600

    JAM-Adding in the code that I wrote for the find largest set of alike characters

---
+++ CSharpCodeTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCodeTest
+{
+	class Program
+	{
+		public static int IndexOfLongestRun(string str)
+		{
+			if(string.IsNullOrEmpty(str))
+			{
+				return -1;
+			}
+			string longestRun = new string(str.Select((c, index) => str.Substring(index).TakeWhile(e => e == c))
+								   .OrderByDescending(e => e.Count())
+								   .First().ToArray());
+			int indexOfLongest = str.IndexOf(longestRun);
+			return indexOfLongest;
+		}
+
+		public static void Main(string[] args)
+		{
+			Console.WriteLine(IndexOfLongestRun("abbcccddddcccbba"));
+			Console.ReadLine();
+		}
+	}
+}
